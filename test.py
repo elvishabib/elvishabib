d5fe10dff605ee3781978599396c3597134f73fc
elvishabib <elvis.akadjame@epitech.eu>
2023-10-10 16:07:11 +0100

    Update Readme

--- test.py
+++ test.py
@@ -1,21 +1,3 @@
-### Hi there 👋
-
-<!--
-**elvishabib/elvishabib** is a ✨ _special_ ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-
-Here are some ideas to get you started:
-
-- 🔭 I’m currently working on ...
-- 🌱 I’m currently learning ...
-- 👯 I’m looking to collaborate on ...
-- 🤔 I’m looking for help with ...
-- 💬 Ask me about ...
-- 📫 How to reach me: ...
-- 😄 Pronouns: ...
-- ⚡ Fun fact: ...
--->
-
-```python
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
@@ -62,4 +44,3 @@ class SoftwareDeveloper:
 
 me = SoftwareDeveloper()
 me.say_hi()
-```
